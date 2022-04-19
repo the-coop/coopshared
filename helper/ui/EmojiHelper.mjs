e1d82128fa32ac1bc4835b56f1ae541a1c584d14
L M Fielding <contact@lmfielding.com>
2022-04-19 04:17:54 +0100

    Improve emojis code

--- helper/ui/EmojiHelper.mjs
+++ helper/ui/EmojiHelper.mjs
@@ -1,21 +1,21 @@
-// const EMOJIS = import('../../config/emojis.json');
-// const EMOJI_BASE_URL = 'https://cdn.discordapp.com/emojis';
+// import EMOJIS from '../../config/emojis.mjs';
+import ITEMS from '../../config/items.mjs';
 
-import EMOJIS from '../../config/emojis.mjs';
+const EMOJI_BASE_URL = 'https://cdn.discordapp.com/emojis';
 
 export default class EmojiHelper {
-    static EMOJIS = EMOJIS;
 
     static idToURL(id, size = 240) {
         return `${EMOJI_BASE_URL}/${id}.webp?size=${size}&quality=lossless`;
     }
     static itemCodeToUnicode(code) {
-        return EMOJIS[code.toUpperCase()].unicode || null;
+        return ITEMS[code.toUpperCase()].unicode || null;
     }
     static itemCodeToEmojiURL(code) {
         let url = null;
 
-        const itemCode = EMOJIS[code.toUpperCase()] || null;
+        const item = ITEMS[code.toUpperCase()] || null;
+        const itemCode = item.id || '';
         const idParts = itemCode.split(':')
         const idPart = idParts[idParts.length - 1];
         const isEmojiID = !isNaN(idPart);
